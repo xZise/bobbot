a630e90358428c0e4089e1675f753d27110500ab
Fabian Neundorf <CommodoreFabianus@gmx.de>
2014-05-03 13:03:12 +0200

    +advance the manually marked thumbs logic;

--- thumbnails.py
+++ thumbnails.py
@@ -53,82 +53,176 @@ VALID_CATEGORIES = { 'Technology tree node images' }
 
 site = pywikibot.getSite()
 
+def find_new_line(string, index, check):
+    if index >= 0:
+        new_line_found = False
+        while check(index, len(string)):
+            new_line = string[index] in ["\n", "\r"]
+            if new_line_found and not new_line:
+                break;
+            elif not new_line_found:
+                new_line_found = new_line
+            start += 1
+
 # read User:BobBot/Thumbnails
-thumb_page = pywikibot.page.Page(site, "User:BobBot/Thumbnails")
-content = thumb_page.text
-thumb_line = re.compile(r"^{{User:BobBot/thumb\|(File:[^|]*)\|([yn?])\|([0-9a-fA-F]{32})}}$", re.M)
-start = content.find("BOBBOT EDIT START") #new line after this
-end = content.find("BOBBOT EDIT END") #new line before this
-entries = thumb_line.findall(content[start:end])
-remaining = []
+thumbs_page = pywikibot.page.Page(site, "User:BobBot/Thumbnails")
+thumbs_content = thumbs_page.text
+thumb_line = re.compile(r"^{{User:BobBot/thumb\|(File:[^|]*)\|([yn?])\|([am])\|([0-9a-fA-F]{32})}}$", re.M)
+hash_regex = re.compile(r"^[0-9a-fA-F]{32}$")
+is_thumb_rep = re.compile("{{([Nn]oThumbnailsPlease|[Nn]o thumbnails please)(\|[0-9a-fA-F]{32})?}}")
+is_no_thumb_rep = re.compile("{{[Ii]s not thumbnail(\|[0-9a-fA-F]{32})?}}")
+start = thumbs_content.find("BOBBOT EDIT START")
+if start >= 0:
+    start = thumbs_content.find("-->", start) #new line after this
+    if start >= 0:
+        new_line_found = False
+        while start < len(thumbs_content):
+            new_line = thumbs_content[start] in ["\n", "\r"]
+            if new_line_found and not new_line:
+                break;
+            elif not new_line_found:
+                new_line_found = new_line
+            start += 1
+end = thumbs_content.find("BOBBOT EDIT END") #new line before this
+if end >= 0 and start >= 0:
+    end = thumbs_content.rfind("<!--", start, end)
+    if end >= 0:
+        while end > 0:
+            if thumbs_content[end - 1] in ["\n", "\r"]:
+                break;
+            end -= 1
+print thumbs_content[start:end]
+entries = thumb_line.findall(thumbs_content[start:end])
+remainings = []
+images_changed = 0
+images_is_template = 0
+images_not_template = 0
+images_updated = 0
+images_already_applied = 0
+images_conflict = 0
 for entry in entries:
     if entry[1] not in ["y", "n"]:
-        remaining += entry
+        remainings += [ entry ]
     else:
+        is_thumbnail = entry[1] == "y"
         image_page = pywikibot.page.ImagePage(site, entry[0])
+        image_data = urllib2.urlopen(image_page.fileUrl()).read()
+        new_hash = hashlib.md5(image_data).hexdigest().lower()
+        is_applicable = new_hash.lower() == entry[3].lower() #hash hashn't changed
+        content = image_page.text
+        templates = textlib.extract_templates_and_params(content)
+        is_thumb_hashes = set()
+        is_no_thumb_hashes = set()
+        for template in templates:
+            hashes = None
+            if template[0].lower() in ["nothumbnailsplease", "no thumbnails please"]:
+                hashes = is_thumb_hashes
+            elif template[0].lower() == "is not thumbnail":
+                hashes = is_no_thumb_hashes
+            if "1" in template[1] and hash_regex.match(template[1]["1"]):
+                hashes.add(template[1]["1"])
+        if len(is_no_thumb_hashes) == 0 and len(is_thumb_hashes) == 0:
+            # automatic
+            if not is_applicable:
+                # hash has changed
+                remainings += [ (entry[0], "?", "a", new_hash) ]
+                images_changed += 1
+            elif is_thumbnail:
+                image.text = "{{No thumbnails please|" + new_hash + "}}\n" + content
+                #image.save(comment="+add 'no thumbnails please' template;")
+                print("'{}' would've saved:".format(image_page.title))
+                print(image.text)
+                images_is_template += 1
+            else:
+                image.text = content + "\n{{Is not thumbnail|" + new_hash + "}}"
+                #image.save(comment="+add 'is not thumbnails' template;")
+                print("'{}' would've saved:".format(image_page.title))
+                print(image.text)
+                images_not_template += 1
+        elif len(is_no_thumb_hashes if is_thumbnail else is_thumb_hashes) == 0 and len(is_thumb_hashes if is_thumbnail else is_no_thumb_hashes) == 1:
+            # there is already a correct entry, but the hash might need to be updated
+            if next(iter(is_thumb_hashes if is_thumbnail else is_no_thumb_hashes)).lower() != new_hash.lower():
+                if is_applicable:
+                    # hash has changed, but the answer was for the current hash
+                    image.text = (is_thumb_rep if is_thumbnail else is_no_thumb_rep).sub(r"{{" + ("No thumbnails please" if is_thumbnail else "Is not thumbnail") + "|" + new_hash.lower() + "}}", content)
+                    #image.save(comment="*update thumbnail template hash;")
+                    print("'{}' would've saved:".format(image_page.title))
+                    print(image.text)
+                    images_updated += 1
+                else:
+                    remainings += [ (entry[0], "?", "a", new_hash) ]
+                    images_changed += 1
+            else:
+                print("Image '{}' is already marked accordingly. No change necessary.".format(image_page.title()))
+                images_already_applied += 1
+        else:
+            # conflict, please resolve manually
+            remainings += [ (entry[0], "?", "c", new_hash) ]
+            images_conflict += 1
 
-print entries
-raise Exception()
+# Files in these categories are small but no thumbnails
+VALID_CATEGORIES = { 'Technology tree node images' }
 
 names = re.compile("^.*\.(?:png|gif|jpg|jpeg)$", re.I)
-yes_all = False
-for arg in pywikibot.handleArgs():
-  if arg in ["--all", "-a"]:
-    yes_all = True
 
+print("Searching for new thumbnails.")
 images_total = 0
-images_marked = 0
+images_unmarked = 0
 images_detected = 0
-images_edited = 0
+images_outdated = 0
+images_multiple = 0
 #Maximum size is 20 KiB (20×2²⁰ bytes)
 for image in site.allimages(maxsize=20*2<<10):
-  images_total += 1
-  if names.match(image.title()):
-    content = image.text
-    templates = textlib.extract_templates_and_params(content)
-    for template in templates:
-      if template[0].lower() in ["nothumbnailsplease", "no thumbnails please"]:
-        print("Already contain template in '{}'. Skipped.".format(image.title()))
-        images_marked += 1
-        break
-      elif template[0].lower() == "is not thumbnail":
-        print("Image '{}' is not a thumbnail. Skipped.".format(image.title()))
-        images_marked += 1
-        break;
+    for remaining in remainings:
+        if remaining[0] == image.title():
+            in_remaining = True
+            break
     else:
-      valid = []
-      for c in image.categories():
-        if c.title(withNamespace=False) in VALID_CATEGORIES:
-          valid += [c.title(withNamespace=False)]
-      if valid:
-        print("Image '{}' is in valid categories '{}'. Skipped.".format(image.title(), "', '".join(valid)))
-        images_marked += 1
-        size = None
-      else:
-        # for didn't find
-        image_data = urllib2.urlopen(image.fileUrl()).read()
-        size = get_image_size(image_data)
-        if size:
-          # the values are switched for the second statement
-          if size[0]/size[1] > 0.4 and size[1]/size[0] > 0.4 and (size[0] < 100 or size[1] < 100):
-            images_detected += 1
-            if not yes_all:
-              answer = pywikibot.inputChoice("Add template to '{}'?".format(image.title()), ['Yes', 'No', 'Mark', 'All'], ['y', 'n', 'm', 'a'], 'n')
-              if answer == 'a':
-                yes_all = True
-            if answer == 'm':
-              image.text = content + "\n{{Is not thumbnail|" + hashlib.md5(image_data).hexdigest() + "}}"
-              image.save(comment="+add 'is not thumbnails' template;")
-              print("Edited page '{}'.".format(image.title()))
-              images_edited += 1
-            elif yes_all or answer != 'n':
-              image.text = "{{No thumbnails please}}\n" + content
-              image.save(comment="+add 'no thumbnails please' template;")
-              print("Edited page '{}'.".format(image.title()))
-              images_edited += 1
+        in_remaining = False
+    if not in_remaining and names.match(image.title()):
+        images_total += 1
+        content = image.text
+        valid = []
+        for c in image.categories():
+            if c.title(withNamespace=False) in VALID_CATEGORIES:
+                valid += [c.title(withNamespace=False)]
+        if valid:
+            print("Image '{}' is in valid category/categories '{}'. Skipped.".format(image.title(), "', '".join(valid)))
         else:
-          print("Unable to determine size of '{}'! Skipped.".format(image.title()))
+            image_data = urllib2.urlopen(image.fileUrl()).read()
+            new_hash = hashlib.md5(image_data).hexdigest().lower()
+            templates = textlib.extract_templates_and_params(content)
+            hashes = set()
+            for template in templates:
+                if (template[0].lower() in ["nothumbnailsplease", "no thumbnails please"] or template[0].lower() == "is not thumbnail") and "1" in template[1] and template[1]["1"] and hash_regex.match(template[1]["1"]):
+                    hashes.add(template[1]["1"])
+            if len(hashes) == 0:
+                images_unmarked += 1
+                size = get_image_size(image_data)
+                if size:
+                    # the values are switched for the second statement
+                    if size[0]/size[1] > 0.4 and size[1]/size[0] > 0.4 and (size[0] < 100 or size[1] < 100):
+                        images_detected += 1
+                        remainings += [ (image.title(), "?", "a", hashlib.md5(image_data).hexdigest()) ]
+                        print("Image '{}' fit's size constraints.".format(image.title()))
+                else:
+                    print("Unable to determine size of '{}'! Skipped.".format(image.title()))
+            elif len(hashes) == 1 and hashes.pop() != new_hash:
+                images_outdated += 1
+                remainings += [ (image.title(), "?", "a", new_hash) ]
+                print("Image '{}' has the thumbnail info set, but with another hash.".format(image.title()))
+            elif len(hashes) > 1:
+                # conflict, please resolve manually
+                images_multiple += 1
+                remainings += [ (entry[0], "?", "c", new_hash) ]
+                print("Image '{}' has multiple thumbnail info entries.".format(image.title()))
 if images_total == 0:
-  print("No images found.")
+    print("No images found.")
 else:
-  print("Statistics: {} images, {} ({:.0%}) already marked, {} ({:.0%}) detected, {} ({:.0%}) edited.".format(images_total, images_marked, images_marked / images_total, images_detected, images_detected / images_total, images_edited, images_edited / images_total))
+    print("Statistics: {} images, {} ({:.0%}) already unmarked, {} ({:.0%}) detected, {} ({:.0%}) outdated, {} ({:.0%}) multiple.".format(images_total, images_unmarked, images_unmarked / images_total, images_detected, images_detected / images_total, images_outdated, images_outdated / images_total, images_multiple, images_multiple / images_total))
+
+# generate new list
+line = "\n".join([ "{{{{User:BobBot/thumb|{}|{}|{}|{}}}}}".format(*remaining) for remaining in remainings ]) + "\n"
+
+thumbs_page.text = thumbs_content[0:start] + line + thumbs_content[end:]
+thumbs_page.save(comment="+{} outdated; +{} multiple; +{} new; *{} changed; *{} conflicts; -{} already applied; -{} 'is thumbnail'; -{} 'is not thumbnail'; -{} hash updated;".format(images_outdated, images_multiple, images_detected, images_changed, images_conflict, images_already_applied, images_is_template, images_not_template, images_updated))
